28fe62947d566bd3a40bdad2929520b4935bba8a
Randy Owens <randyowens2019@yahoo.com>
2023-04-04 03:23:11 -0400

    Initial Project Commit

---
+++ seniorproject/src/main/java/randyowens/seniorproject/SeniorprojectApplication.java
@@ -0,0 +1,13 @@
+package randyowens.seniorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeniorprojectApplication.class, args);
+	}
+
+}
---
+++ seniorproject/src/test/java/randyowens/seniorproject/SeniorprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package randyowens.seniorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
