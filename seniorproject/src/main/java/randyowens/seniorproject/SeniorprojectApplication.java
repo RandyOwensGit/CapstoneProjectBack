43d79aa435a2280e59e99179d4ccf7450e5b8f28
Randy Owens <randyowens2019@yahoo.com>
2023-04-17 23:40:03 -0400

    Docker File

--- seniorproject/src/main/java/randyowens/seniorproject/SeniorprojectApplication.java
+++ seniorproject/src/main/java/randyowens/seniorproject/SeniorprojectApplication.java
@@ -2,12 +2,20 @@ package randyowens.seniorproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SeniorprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeniorprojectApplication.class, args);
+	}
+
+	@GetMapping("count-users")
+	public String getCountUsers() {
+		return "Hello Render with Spring Boot";
 	}
 
 }
